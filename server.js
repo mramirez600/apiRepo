14fcb93c667a1fbf200440d632c792028a8ac39d
Marvin Ramirez <eldo@hotmail.com>
2019-11-03 09:25:26 -0500

    models commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -24,8 +25,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(
+  PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`
-  );
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
+      .bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server and exit process
+  server.close(() => process.exit(1));
 });
