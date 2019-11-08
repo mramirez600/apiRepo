5882be163b7a96c9821e66179b45e0c2e87d7690
Marvin Ramirez <eldo@hotmail.com>
2019-11-08 08:53:38 -0500

    add course commit

--- routes/course.js
+++ routes/course.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { getCourses } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse } = require('../controllers/courses');
 
 const Router = express.Router({ mergeParams: true });
 
-Router.route('/').get(getCourses);
+Router.route('/')
+  .get(getCourses)
+  .post(addCourse);
+Router.route('/:id').get(getCourse);
 
 module.exports = Router;
