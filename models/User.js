3c5fa45040c7ed7c4a64f7bd6e7ab2ed93a389cc
Marvin Ramirez <eldo@hotmail.com>
2019-11-09 10:51:48 -0500

    user model commit

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  role: {
+    type: String,
+    enum: ['user', 'publisher'],
+    default: 'user'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false
+  },
+  resetPasswordToken: String,
+  resetPasswordExp: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
