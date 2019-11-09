8b9f635c7ac409166e9542651f66ed3c055e1b3a
Marvin Ramirez <eldo@hotmail.com>
2019-11-09 14:24:51 -0500

    user registration and password encrypt commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,20 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
-const Bootcamp = require('../models/User');
+const User = require('../models/User');
 
 // @desc   Register user
 // @route  GET api/auth/register
 // @access Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
   res.status(200).json({ success: true });
 });
