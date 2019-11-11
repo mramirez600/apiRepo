b65875bda57de564ed3157ed734d38e5043f174f
Marvin Ramirez <eldo@hotmail.com>
2019-11-11 09:00:30 -0500

    reviews and ratings commit

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const bootcamps = require('./routes/bootcamp');
 const courses = require('./routes/course');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
@@ -44,6 +45,7 @@ app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
 
 // Error Handler
 app.use(errorHandler);
